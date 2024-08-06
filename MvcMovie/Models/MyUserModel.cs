2aadd8898b103cf06819463089281ca0f20dd34c
JadeYookJunnie <jade.lemonade2@gmail.com>
2024-08-07 11:53:11 +1200

    updating classes (based on uml)

---
+++ MvcMovie/Models/MyUserModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MvcMovie.Models
+{
+    public class MyUserModel : UserModel
+    {
+        public string Password { get; set; }
+
+        public void addFriend(UserModel user)
+        {
+
+        }
+
+        public void removeFriend(UserModel user) { }
+
+        public void addBookToWishlist(BookModel book) { }
+        public void addBookToFavorites(BookModel book) { }
+        public void addBookToCurrentReads(BookModel book) { }
+        public void removeBookFromWishlist(BookModel book) { }
+        public void removeBookFromFav(BookModel book) { }
+        public void removeBookFromCurrReads(BookModel book) { }
+
+
+    }
+}
