35cf9c81d5cfb932f3a661a2988d71056cdbbd18
Shean Danes <sheandanes1978@gmail.com>
2024-08-08 11:34:15 +1200

    Set up of sass to extend variables

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96bc6890c042e24ca78a8bf4bbf2ac96a73f62e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a2221011bf6009a3f3ee77abfcb70dcb5dacff9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
