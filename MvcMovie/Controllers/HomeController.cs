5e2995cb58d8eb6c541c74be209a57edddce5c56
Joethy <michaelpeddie389@gmail.com>
2024-08-19 16:05:44 +1200

    Set up AWS Cognito, added link(s) for signing in and up, admin user exists in comments in home controller
    Added node_modules to gitignore so we dont have to keep deleting it before committing

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -6,6 +6,12 @@ namespace MvcMovie.Controllers
 {
     public class HomeController : Controller
     {
+
+        /* SIGN IN INFORMATION FOR EXISTING ACCOUNT COS I FUCKED UP SIGN UP PROCESS:
+         * username: admin
+         * password: Adm!n1strator
+         */
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
