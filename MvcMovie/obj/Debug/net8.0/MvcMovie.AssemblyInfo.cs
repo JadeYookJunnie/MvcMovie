20c331c66915506b2467a5543563e3fb2208f086
JadeYookJunnie <jade.lemonade2@gmail.com>
2024-09-01 14:21:38 +1200

    assembly

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b28917e2d0fe12543457f9e4cbd430eee58b11f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef89eecd08a55dbbdfbeaeac25e882b7ad71929")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
