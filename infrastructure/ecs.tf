ddcd38989d86d839c9f9cf1bd94c1e632cecf6f7
EthanMacleodUni <ethandavidfrancis@gmail.com>
2024-09-02 09:19:01 +1200

    Fixed login redirect url

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -24,12 +24,6 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
       cpu       = 256
       memory    = 512
       essential = true
-      secrets = [
-        {
-          name      = "BOOK_API"
-          valueFrom = var.book_api_arn
-        }, 
-      ]
       portMappings = [
         {
           containerPort = 8080
