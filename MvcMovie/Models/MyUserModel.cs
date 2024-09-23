0a8f5554daae4f679119493d2ba6822bfbbc3e21
Reid, Abbie <ar233@students.waikato.ac.nz>
2024-09-23 14:22:03 +1200

    More notes for implementation

--- MvcMovie/Models/MyUserModel.cs
+++ MvcMovie/Models/MyUserModel.cs
@@ -1,7 +1,14 @@
-﻿namespace MvcMovie.Models
+﻿﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon;
+using MvcMovie.Services;
+
+namespace MvcMovie.Models
 {
     public class MyUserModel : UserModel
     {
+        public static AmazonDynamoDBClient dbClient;
+
         public string Name {get; set;}
         public string Password { get; set; }
         public List<BookModel> Favourites { get; set; } = new List<BookModel>(); 
@@ -10,13 +17,108 @@
 
         public List<ReviewModel> ReviewList { get; set; } = new List<ReviewModel>();
 
-    public MyUserModel(string name, string password)
+        // have made these string lists for now so that the data from db can be stored easily
+        public static List<string> StrFriends;
+        public static List<string> StrFavourites;
+        public static List<string> StrCurrentReads;
+        public static List<string> StrWishlist;
+        public static List<string> StrBooksReviewed;
+
+        public MyUserModel(string name)
         {
             Name = name;
-            Password = password; 
-            CurrentReads = new List<BookModel>(); 
-            Favorites = new List<BookModel>();    
+
+            dbClient = dynamoDBConnect();
+
+            // populates the local lists with data from db
+            GetData(name);
+
+            // CurrentReads = new List<BookModel>(); 
+            // Favorites = new List<BookModel>();   
+
+            StrFriends = new List<string>();
+            StrWishlist = new List<string>();
+            StrFavourites = new List<string>();
+            StrCurrentReads = new List<string>(); 
+            StrBooksReviewed = new List<string>();
         }
+
+        public AmazonDynamoDBClient dynamoDBConnect(){
+            string accessKey = Environment.GetEnvironmentVariable("DB_ACCESS_KEY");
+            string secretKey = Environment.GetEnvironmentVariable("DB_SECRET_ACCESS_KEY");
+
+            AmazonDynamoDBClient client;
+
+            // create the config item to specify region
+            AmazonDynamoDBConfig clientConfig = new AmazonDynamoDBConfig();
+            clientConfig.RegionEndpoint = RegionEndpoint.USEast1;
+
+            client = new AmazonDynamoDBClient(accessKey, secretKey, clientConfig);
+
+            return client;
+        }
+
+        public static async Task<bool> GetData(string username)
+        {
+            string tableName = "BR_User";
+
+            var key = new Dictionary<string, AttributeValue>
+            {
+                ["username"] = new AttributeValue { S = username },
+            };
+
+            var request = new GetItemRequest
+            {
+                Key = key,
+                TableName = tableName,
+            };
+
+            var response = await dbClient.GetItemAsync(request);
+
+            if(response.Item == null){
+                return false;
+            }
+
+            foreach(var attribute in response.Item){
+                if(attribute.Key == "friends"){
+                    StrFriends = attribute.Value.SS;
+                }
+                else if(attribute.Key == "favourites"){
+                    StrFavourites = attribute.Value.SS;
+                }
+                else if(attribute.Key == "currentreads"){
+                    StrCurrentReads = attribute.Value.SS;
+                }
+                else if(attribute.Key == "wishlist"){
+                    StrWishlist = attribute.Value.SS;
+                }
+            }
+
+            // Console.WriteLine("favourites:");
+            // foreach(string item in StrFavourites){
+            //     Console.WriteLine(item);
+            // }
+            
+            return true;
+        }
+
+        public async void IdsToBookModel(GoogleBooksService booksService){
+            // here the string lists need to be turned into the BookModel lists
+            // so that it can be used in MyUser.cshtml
+
+            foreach (string bookId in StrCurrentReads){
+                string query = "";
+                var books = await booksService.SearchAllBooksAsync(query);
+                // somehow here then books is used to create a BookModel for the book
+                // this BookModel then gets added to List<BookModel> CurrentReads
+            }
+
+            // repeat the foreach for favourites
+
+            // repeat the foreach for wishlist
+
+            // note: not here but will probably have to turn StrReviews into the ReviewModel
+        }   
 
         public void addFriend(UserModel user)
         {
