a83ecb8fa86c88367ff4ccf3ce8c51089f6d89dc
Reid, Abbie <ar233@students.waikato.ac.nz>
2024-09-25 10:32:28 +1200

    BookModel gets reviews

--- MvcMovie/Models/BookModel.cs
+++ MvcMovie/Models/BookModel.cs
@@ -1,7 +1,16 @@
-﻿namespace MvcMovie.Models
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon;
+using System.Globalization;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MvcMovie.Models
 {
     public class BookModel
     {   
+        public static AmazonDynamoDBClient dbClient;
         public string ISBN { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
@@ -26,6 +35,9 @@
             this.ReviewCount = 0;
             this.BookReviews = new List<ReviewModel>();
             this.PublishDate = PublishDate;
+
+            dbClient = dynamoDBConnect();
+            GetReviews();
         }
         public void addReview(ReviewModel review)
         {  
@@ -34,6 +46,80 @@
         public void RemoveReview(ReviewModel review)
         {  
             BookReviews.Remove(review);
+        }
+
+        public AmazonDynamoDBClient dynamoDBConnect(){
+            string accessKey = Environment.GetEnvironmentVariable("DB_ACCESS_KEY");
+            string secretKey = Environment.GetEnvironmentVariable("DB_SECRET_ACCESS_KEY");
+
+            AmazonDynamoDBClient client;
+
+            // create the config item to specify region
+            AmazonDynamoDBConfig clientConfig = new AmazonDynamoDBConfig();
+            clientConfig.RegionEndpoint = RegionEndpoint.USEast1;
+
+            client = new AmazonDynamoDBClient(accessKey, secretKey, clientConfig);
+
+            return client;
+        }
+
+        public async Task<bool> GetReviews(){
+
+            List<ReviewModel> reviews = new List<ReviewModel>();
+
+            var request = new QueryRequest
+            {
+                TableName = "BR_Reviews",
+                KeyConditionExpression = "book = :v_book",
+                ExpressionAttributeValues = new Dictionary<string, AttributeValue> {
+                    {":v_book", new AttributeValue { S =  ISBN }}}
+            };
+
+            var response = await dbClient.QueryAsync(request);
+
+            // temp values to store response
+            DateTime date = new DateTime();
+            int rating = 0;
+            string book = "";
+            string reviewID = "";
+            string review = "";
+            int likeCount = 0;
+            string user = "";
+
+            string datePattern = "yyyyMMdd";
+
+            foreach (Dictionary<string, AttributeValue> item in response.Items)
+            {
+                foreach (KeyValuePair<string, AttributeValue> pair in item){
+                    if(pair.Key == "book"){
+                        book = pair.Value.S;
+                    }
+                    else if(pair.Key == "reviewID"){
+                        reviewID = pair.Value.S;
+                    }
+                    else if(pair.Key == "date"){
+                        DateTime.TryParseExact(pair.Value.S, datePattern, null, DateTimeStyles.None, out date);
+                    }
+                    else if(pair.Key == "likecount"){
+                        likeCount = int.Parse(pair.Value.N);
+                    }
+                    else if(pair.Key == "rating"){
+                        rating = int.Parse(pair.Value.N);
+                    }
+                    else if(pair.Key == "review"){
+                        review = pair.Value.S;
+                    }
+                    else if(pair.Key == "user"){
+                        user = pair.Value.S;
+                    }
+                }
+                
+                // create review model from these values
+                ReviewModel reviewModel = new ReviewModel(1, reviewID, null, this, likeCount, rating, review, date);
+            } 
+
+            BookReviews = reviews;
+            return true;
         }
     }
 }
